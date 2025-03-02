57943f003e0b8eab45bb11f179a487c80d5c1bda
Amartya Bose <amartya.bose@gmail.com>
2025-03-02 18:43:43 +0530

    docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(
     modules=[QuantumDynamicsCLI],
     sitename="QuantumDynamicsCLI.jl",
     pages=[
-        "Bose Research Group" => "https://amartyabose.github.io/"
+        "Bose Research Group" => "https://amartyabose.github.io/",
         "Introduction" => "index.md",
         "Documentation" => [
             "Simulate Module" => "./documentation/Simulate.md",
